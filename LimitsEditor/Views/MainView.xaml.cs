88a3fd59822075daf765550bb179f597d4f43fe0
EdenH <74952843+EdenH442@users.noreply.github.com>
2026-03-07 15:24:33 +0200

    Refactor WPF shell to MVVM Toolkit and modern layout

--- LimitsEditor/Views/MainView.xaml.cs
+++ LimitsEditor/Views/MainView.xaml.cs
@@ -1,22 +1,13 @@
 using System.Windows;
-using LimitsEditor.Services;
-using LimitsEditor.Validation;
 using LimitsEditor.ViewModels;
 
 namespace LimitsEditor.Views;
 
 public partial class MainView : Window
 {
-    public MainView()
+    public MainView(MainViewModel viewModel)
     {
         InitializeComponent();
-
-        // TODO: Move to app-level dependency injection composition root.
-        DataContext = new MainViewModel(
-            new JsonFileService(),
-            new BackupService(),
-            new JsonUpsertService(),
-            new FileValidationService(),
-            new TestItemValidator());
+        DataContext = viewModel;
     }
 }

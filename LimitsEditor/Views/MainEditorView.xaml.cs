ab8017e2b7bc4d0d8687313e79dcf3628113fe57
EdenH <74952843+EdenH442@users.noreply.github.com>
2026-08-20 07:44:43 +0300

    Toggle test details on repeated selection

--- LimitsEditor/Views/MainEditorView.xaml.cs
+++ LimitsEditor/Views/MainEditorView.xaml.cs
@@ -1,5 +1,7 @@
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 using LimitsEditor.ViewModels;
 
 namespace LimitsEditor.Views;
@@ -9,6 +11,37 @@ public partial class MainEditorView : UserControl
     public MainEditorView()
     {
         InitializeComponent();
+    }
+
+    private void TestsListBox_OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (sender is not ListBox listBox ||
+            DataContext is not MainEditorViewModel viewModel ||
+            FindAncestor<Button>(e.OriginalSource as DependencyObject) is not null ||
+            ItemsControl.ContainerFromElement(listBox, e.OriginalSource as DependencyObject) is not ListBoxItem item ||
+            item.DataContext is not TestNavigationItemViewModel clickedTest ||
+            !ReferenceEquals(clickedTest, viewModel.SelectedTestItem))
+        {
+            return;
+        }
+
+        viewModel.SelectedTestItem = null;
+        e.Handled = true;
+    }
+
+    private static T? FindAncestor<T>(DependencyObject? current) where T : DependencyObject
+    {
+        while (current is not null)
+        {
+            if (current is T match)
+            {
+                return match;
+            }
+
+            current = VisualTreeHelper.GetParent(current);
+        }
+
+        return null;
     }
 
     private void SequenceNameTextBox_OnLostFocus(object sender, RoutedEventArgs e)
